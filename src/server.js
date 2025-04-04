fdc8f4b9126ba1df0a74915b00fa523e1bc9bd3f
le cong hien <leconghienlch2006@gmail.com>
2025-04-04 18:27:08 +0700

    .env

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
+const { log } = require("console");
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT ?? 8080;
+const hostName = process.env.HOST_NAME;
 
+console.log(process.env);
+console.log(hostName);
 // config template engine ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -11,6 +16,6 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.render("example.ejs");
 });
-app.listen(port, () => {
+app.listen(port, hostName, () => {
     console.log(`Example app listening on port ${port}`);
 });
