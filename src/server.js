b798dfb86bcc0c036b908ec0cf3a723629e6f1bf
le cong hien <leconghienlch2006@gmail.com>
2025-04-04 17:41:18 +0700

    view engine

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-    res.send("Hello World hien it");
-});
+// config template engine ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// khai báo route
 
+app.get("/", (req, res) => {
+    res.render("example.ejs");
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
